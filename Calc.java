0b02e4dc0f955839f4a488207702f1d04ce34625
vivek <vivekbachche99@gmail.com>
2025-10-15 18:51:44 +0530

    calcu

---
+++ Calc.java
@@ -0,0 +1,20 @@
+public class Calc {
+
+
+    public static void main(String[] args) {
+        c c= new c();
+        System.out.println(""+c.add(10, 020));
+    }
+}
+
+
+class c{
+
+public int add(int a,int b){
+
+return a+b;
+
+}
+
+
+}
